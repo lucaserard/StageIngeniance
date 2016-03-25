394a34ec02e07b9b59c2209b2b5bf146d430669f
Lucas <lucas.erard@insa-rouen.fr>
2016-03-25 12:11:27 +0100

    suppression de commentaires et micro refactor

--- scriptJSON/jsonToCSV.php
+++ scriptJSON/jsonToCSV.php
@@ -2,11 +2,11 @@
 
 	class Card{
 
-		public $intitule;
-		public $description;
-		public $process="";
-		public $priorite=0;
-		public $liste;
+		private $intitule;
+		private $description;
+		private $process="";
+		private $priorite=0;
+		private $liste;
 
 		public function setPriorite(){
 
@@ -38,6 +38,26 @@
 			$this->liste=utf8_encode($liste);
 		}
 
+		public function getIntitule(){
+			return $this->intitule;
+		}
+
+		public function getDescription(){
+			return $this->description;
+		}
+
+		public function getProcess(){
+			return $this->process;
+		}
+
+		public function getPriorite(){
+			return $this->priorite;
+		}
+
+		public function getListe(){
+			return $this->liste;
+		}
+
 	}	
 
 	$jsonPage = file_get_contents('/home/lucas/Documents/Boulot/Test JSON/trello.json');
@@ -62,45 +82,18 @@
 		$card->setPriorite();
 		$card->setListe($listsArray[$story->{'idList'}]);
 
-		// $card->intitule=utf8_encode($story->{'name'});
-		// $card->description=utf8_encode($story->{'desc'});
-		// foreach ($story->{'labels'} as $label) {
-			// $card->process=$card->process.", ".$label->{'name'};
-		// }
-		// $card->process=utf8_encode($card->process);
-		// $card->liste=utf8_encode($listsArray[$story->{'idList'}]);
 
-		// var_dump($card);
-
-
-		$exportArray[]=array($card->intitule,$card->description,$card->process,intval($card->priorite),$card->liste);
-
-		$fp = fopen('stories.csv', 'w');
-		fputs($fp, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
-		foreach ($exportArray as $line) {
-			$line = array_map("utf8_decode", $line);
-    		fputcsv($fp, $line,';');
-		}
-
-		fclose($fp);
-		// var_dump($exportArray);
-
-
+		$exportArray[]=array($card->getIntitule(),$card->getDescription(),$card->getProcess(),intval($card->getPriorite()),$card->getListe());
 
 	}
 
-	// var_dump($stories);
-	// foreach ($trello as $key => $value){
-		// echo "key :".$key."\n";
-	// }	
-	// 	// echo "key :".$key.", "."value :".$value."\n";		
-	// // 	$i++;
-	// // 	foreach ($stuff as $key => $value) {
-	// // 		// echo "key :".$key.", "."value :".$value."\n";
-	// // 		echo "key :".$key."\n";
-	// // 	}
+	$fp = fopen('stories.csv', 'w');
+	fputs($fp, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
+	foreach ($exportArray as $line) {
+		$line = array_map("utf8_decode", $line);
+		fputcsv($fp, $line,';');
+	}
 
-	// // 	echo "i : ".intval($i);
-
+	fclose($fp);
 		
 ?>
