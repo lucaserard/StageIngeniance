01880f59e9d8c9b8aa2df98189ff3c35bac46544
Lucas <lucas.erard@insa-rouen.fr>
2016-04-04 18:32:02 +0200

    Architecture BPM

---
+++ config.sh
@@ -0,0 +1,3 @@
+Ingeniance=$('pwd')
+echo "export Ingeniance=$Ingeniance">> ~/.bashrc
+echo "Config terminée : Veuillez relancer votre terminal avec la commande cd;. .bashrc;cd -; ou bien en ouvrir un nouveau pour continuer "
